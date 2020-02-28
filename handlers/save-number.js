6d31c377b13788a91038e160b8992ba014156c35
Mykola Marenich <marenich.mykola@gmail.com>
2020-02-28 19:57:53 +0100

    save-number

--- handlers/save-number.js
+++ handlers/save-number.js
@@ -1,19 +1,27 @@
 function saveNumberHandler() {
   debugger;
   // read new number from user input
+  let newNumber = document.getElementById('input').value;
 
   // read from state the data you will need for the next step
+  numbers.all.push(newNumber);
 
   // find the new biggest and smallest values
+  let newBiggest = numbers.all.reduce((a, b) => Math.max(a, b));
+  let newSmallest = numbers.all.reduce((a, b) => Math.min(a, b));
 
   // update state: new biggest, new smallest, new current & save the last current
+  numbers.biggest = newBiggest;
+  numbers.smallest = newSmallest;
 
   // re-render the user interface with values stored in state
+  document.getElementById("biggest").innerText = newBiggest;
+  document.getElementById("smallest").innerText = newSmallest;
 
   // log user interaction: handler name, user input, new state
   log.push({
     handler: 'save number',
-    newInput,
+    newInput: newNumber,
     numbers: JSON.parse(JSON.stringify(numbers))
   })
 }
